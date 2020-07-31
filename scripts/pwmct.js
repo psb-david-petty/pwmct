6e0fc9180d7124dc11511cd3916346e507e9b46c
psb-david-petty <psb-david-petty@users.noreply.github.com>
2020-07-31 07:56:34 -0400

    Fixed .JS for <article> w/ no id. Adjusted question number width.

--- scripts/pwmct.js
+++ scripts/pwmct.js
@@ -30,12 +30,22 @@ function shuffle(array) {
   return array;
 }
 
-/* Collect elements of selector that match children of article:
+/* Collect elements of selector that match children of <article id=:
  * <a href=
  * <a title=
  * <img src=
  * <img alt=
  * ...then shuffle them and rewrite them in order of article ids.
+ *
+ * The format of an example article question is:
+    <article id="whatever-it-is">
+      <div>
+        <!-- div contains multiple choices... -->
+      </div>
+      <a href="./doc/pwmct.pdf" title="PERSON WOMAN MAN CAMERA TV">
+        <img src="./images/wtf.png" alt="WTF?!">
+      </a>
+    </article>
  */
 function onLoad() {
     let ids = new Array();
@@ -44,6 +54,7 @@ function onLoad() {
 
     /* Collect id and then information from children of article. */
     for (let node of document.querySelectorAll(article)) {
+        if (!node.id) continue;         // article must have id
         ids.push(node.id);              // save article ids in order
         let question = new Object();    // create new question object
         question.id = node.id;
